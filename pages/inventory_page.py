cf6f2052226afa637724149114e2df241c65706d
Israel Garcia <isragadiel@hotmail.com>
2026-06-19 22:35:40 -0300

    errores corregidos

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 class InventoryPage(BasePage):
@@ -16,8 +18,27 @@ class InventoryPage(BasePage):
         return self.obtener_texto(self.LBL_TITULO_PAGINA)
 
     def agregar_productos_al_carrito(self):
+        # Agregar mochila
         self.hacer_clic(self.BTN_AGREGAR_MOCHILA)
+
+        # Esperar que el contador muestre 1
+        WebDriverWait(self.driver, 10).until(
+            EC.text_to_be_present_in_element(
+                self.LBL_CONTADOR_CARRITO,
+                "1"
+            )
+        )
+
+        # Agregar remera
         self.hacer_clic(self.BTN_AGREGAR_REMERA)
+
+        # Esperar que el contador muestre 2
+        WebDriverWait(self.driver, 10).until(
+            EC.text_to_be_present_in_element(
+                self.LBL_CONTADOR_CARRITO,
+                "2"
+            )
+        )
 
     def obtener_cantidad_carrito(self):
         return self.obtener_texto(self.LBL_CONTADOR_CARRITO)
